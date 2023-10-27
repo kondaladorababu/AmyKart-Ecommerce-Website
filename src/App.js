c9028fd5b9cf8d2ec2ebf797968ff35589acfa1e
Dora Kondala <kondaladorababu@gmail.com>
2023-10-27 21:00:00 +0530

    Home Banner added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route exact path='/' element={<Header />}></Route>
-          <Route exact path='/login' element={<h1>Login</h1>}></Route>
-          <Route exact path='/checkout' element={<h1>checkout</h1>}>
+          <Route exact path='/' element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }></Route>
+
+          <Route exact path='/login' element={
+            <>
+              <Header />
+              <h1>Login</h1>
+            </>
+          }></Route>
+
+          <Route exact path='/checkout' element={
+            <>
+              <Header />
+              <h1>checkout</h1>
+            </>
+          }>
           </Route>
+
         </Routes>
       </div >
     </BrowserRouter >
