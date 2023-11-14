a29dc2dfc29bbd34c93e3d62c5c196ada8b23902
Dora Kondala <kondaladorababu@gmail.com>
2023-11-14 22:11:34 +0530

    New firebase account linked with new project id

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBUgXVc-hFCTpLzZaH9jowg3hqX-LalxCI",
-    authDomain: "clone-5379b.firebaseapp.com",
-    projectId: "clone-5379b",
-    storageBucket: "clone-5379b.appspot.com",
-    messagingSenderId: "198619341728",
-    appId: "1:198619341728:web:6081b1dcc0b29eea87fac5",
-    measurementId: "G-ZC4G2GR20E"
+    apiKey: "AIzaSyAgnqXqoRbxiPSsvfv1_DX24x7iQOWT538",
+    authDomain: "clone-70561.firebaseapp.com",
+    projectId: "clone-70561",
+    storageBucket: "clone-70561.appspot.com",
+    messagingSenderId: "110574297426",
+    appId: "1:110574297426:web:55bac4ab6e4fe1189e9a8b",
+    measurementId: "G-JVTDP73Q98"
 };
 
 const app = initializeApp(firebaseConfig);
