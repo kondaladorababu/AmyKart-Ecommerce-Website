d0105f96b3188733ffc83a7704ff5b9d072ed497
Dora Kondala <kondaladorababu@gmail.com>
2023-12-06 21:32:33 +0530

    Cart Page/Checkout is redesigned

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import '../styles/Checkout.css';
 import { useStateValue } from '../Store/StateProvider';
 import CheckOutProduct from './CheckOutProduct';
@@ -7,45 +7,45 @@ import CheckTotal from './CheckTotal';
 
 function Checkout() {
     const { state } = useStateValue();
-    const { basket, user } = state;
+    const { basket } = state;
 
     return (
         <div className='checkout'>
-            <div className="checkout_left">
-                <img className='checkout_add'
-                    src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/AmazonPay/Jupiter23/Wave3/Train_GW_editorial_2300x646._CB575359240_.jpg"
-                    alt=""
-                />
-                {basket?.length === 0 ?
-                    (
-                        <div>
-                            <h2>Your Basket is Empty</h2>
-                            {!user ? <p>Add Products to see in Your Basket</p> : <p>You Have no Items in Your Basket</p>}
+            {basket?.length === 0 ?
+                (
+                    <div className='empty_basket_title'>
+                        <h2>Your Basket is Empty</h2>
+                        <p>Add Products to see in Your Basket</p>
+                    </div>
+                ) :
+                (
+                    <Fragment>
+                        <div className="checkout_left">
+                            <div>
+                                <h2 className='checkout_title'>Items in your Shopping Cart</h2>
+                                {basket.map((item, index) => (
+                                    <CheckOutProduct
+                                        key={index}
+                                        id={item.id}
+                                        title={item.title}
+                                        image={item.image}
+                                        price={item.price}
+                                        quantity={item.quantity}
+                                    />
+                                ))}
+                            </div>
                         </div>
-                    ) : (
-                        <div>
-                            <h2 className='checkout_title'>Items in your Shopping Cart</h2>
-                            {basket.map((item, index) => (
-                                <CheckOutProduct
-                                    key={index}
-                                    id={item.id}
-                                    title={item.title}
-                                    image={item.image}
-                                    price={item.price}
-                                    quantity={item.quantity}
-                                />
-                            ))}
-                        </div>
-                    )}
-            </div>
 
-            {basket.length > 0 && (
-                <div className="checkout_right">
-                    <CheckTotal />
-                </div>
-            )}
+                        <div className="checkout_right">
+                            <h2 className='checkout_title'>Items Summary</h2>
+                            <CheckTotal />
+                        </div>
+                    </Fragment>
+                )}
+
+
         </div>
     )
 }
 
-export default Checkout
+export default Checkout;
